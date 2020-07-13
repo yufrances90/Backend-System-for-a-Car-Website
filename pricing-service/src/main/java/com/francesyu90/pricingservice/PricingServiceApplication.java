f93ff97bbe531410469f667d3e4e2222c537c88e
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-13 11:53:58 -0700

    feat: Resolved #1 #2

--- pricing-service/src/main/java/com/francesyu90/pricingservice/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/francesyu90/pricingservice/PricingServiceApplication.java
@@ -2,8 +2,10 @@ package com.francesyu90.pricingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
 	public static void main(String[] args) {
