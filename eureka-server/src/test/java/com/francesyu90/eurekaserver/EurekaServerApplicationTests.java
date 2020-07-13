9623361f4839cad4bcc75d62e838f99d92150f44
Beijia(Frances) Yu <43866367+yufrances90@users.noreply.github.com>
2020-07-13 09:36:24 -0700

    *

---
+++ eureka-server/src/test/java/com/francesyu90/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francesyu90.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
